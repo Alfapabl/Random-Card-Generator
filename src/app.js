61eeece2b05ecb4673b6e605d6fe4a549c5b4452
Pablo Méndez <pablo-mendez09@hotmail.com>
2022-08-17 18:21:11 +0000

    Proyecto Completo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "k"];
+let images = ["♦", "♥", "♠", "♣"];
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let variable_values = [Math.floor(Math.random() * values.length)];
+  let variables_images = [Math.floor(Math.random() * images.length)];
+
+  let header = document.querySelector("#header");
+  let center = document.querySelector("#center");
+  let footer = document.querySelector("#footer");
+
+  header.innerHTML = images[variables_images];
+  center.innerHTML = values[variable_values];
+  footer.innerHTML = images[variables_images];
 };
+
+onload();
