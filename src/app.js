daf3950f46a7b32034e944bf65ce2d2574de6882
Pablo Méndez <pablo-mendez09@hotmail.com>
2022-08-17 22:06:08 +0000

    added colors

--- src/app.js
+++ src/app.js
@@ -1,19 +1,27 @@
 let values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "k"];
 let images = ["♦", "♥", "♠", "♣"];
 
+let header = document.querySelector("#header");
+let center = document.querySelector("#center");
+let footer = document.querySelector("#footer");
+
+function color(icon) {
+  if (icon == "♥" || icon == "♦") {
+    header.style.color = "red";
+    footer.style.color = "red";
+  }
+}
+
 window.onload = function() {
   //write your code here
 
   let variable_values = [Math.floor(Math.random() * values.length)];
   let variables_images = [Math.floor(Math.random() * images.length)];
 
-  let header = document.querySelector("#header");
-  let center = document.querySelector("#center");
-  let footer = document.querySelector("#footer");
-
   header.innerHTML = images[variables_images];
   center.innerHTML = values[variable_values];
   footer.innerHTML = images[variables_images];
+  color(images[variables_images]);
 };
 
 onload();
